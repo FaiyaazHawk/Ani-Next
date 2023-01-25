fa51f64b1db638d37acf0c23d752ed02fdd46178
Faiyaaz <faiyaaz.haque90@gmail.com>
2023-01-25 13:29:06 -0500

    got the player to select 1080p stream and play showing controls

--- src/Components/EpisodePage.tsx
+++ src/Components/EpisodePage.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import '../Styles/EpisodePage.css'
 
 const EpisodePage = () => {
 
-  const [episodeInfo, setEpisodeInfo] =React.useState([])
+  const [episodeInfo, setEpisodeInfo] =React.useState<any>([])
+  
 
   let params = useParams()
 
   let EPISODE_STRING = `https://api.consumet.org/anime/gogoanime/watch/${params.id}`
 
+  interface xTypes{
+    quality:string;
+  }
+
   const fetchEpisodesources = async () => {
     await axios.get(EPISODE_STRING)
     .then((response) => {
-      setEpisodeInfo(response.data.sources)
+      const data = Object.values(response.data.sources).filter((episode:any) => episode.quality === 'default') //pulling the link for 1080p stream from data
+      setEpisodeInfo(data)
+      
   }).catch((error) => console.log(error))
   }
 
@@ -21,10 +30,19 @@ const EpisodePage = () => {
     fetchEpisodesources();
   }, [])
 
-  console.log(episodeInfo)
+  interface episodeTypes {
+    url:string;
+    quality:string;
+  }
+
+  const videoElements = episodeInfo.map((episode:episodeTypes)=> {
+    return <ReactPlayer controls={true} key={episode.quality} url={episode.url}></ReactPlayer>
+  })
 
   return (
-    <div>EpisodePage</div>
+    <div className='video-player'>
+      {episodeInfo && videoElements}
+    </div>
   )
 }
 
