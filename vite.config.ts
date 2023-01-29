42f09070dfd4fb80b5bd47233dea630ce71785e1
Faiyaaz <faiyaaz.haque90@gmail.com>
2023-01-29 08:40:39 -0500

    updated config file for manifest

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,27 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-    VitePWA({ registerType: 'autoUpdate' },
+    VitePWA({ registerType: 'autoUpdate', 
+    manifest: {
+      name: 'My Awesome App',
+      short_name: 'MyApp',
+      description: 'My Awesome App description',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'public/AppImages/android/android-launchericon-192-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'public/AppImages/android/android-launchericon-512-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        }
+      ]
+    }}
     )],
 })
 
