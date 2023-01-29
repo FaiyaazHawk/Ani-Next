37fc1659192a74390cb89b7c35e0ecea73a31a1e
Faiyaaz <faiyaaz.haque90@gmail.com>
2023-01-29 08:27:22 -0500

    pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-    VitePWA({ registerType: 'autoUpdate' })],
+    VitePWA({ registerType: 'autoUpdate' },
+    )],
 })
 
 
